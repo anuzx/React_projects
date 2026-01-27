22ef8b413e198ea12340a5eeed58b271edc3e95b
anuzx <anujparasar4266@gmail.com>
2026-01-28 03:34:45 +0530

    counter using custom hook

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,12 +1,13 @@
+import { useCounter } from "./hooks/useCounter.js"
 
+//when we use default with export we dont need to use {} , but when we dont use export default , we need to use {} to import 
 
 
 function App() {
+  const {count , increaseCount} = useCounter()
   return (
     <div>
-      <b>counter</b>
-
-      <Counter></Counter>
+      <button onClick={increaseCount}>Increase {count}</button>
     </div>
   );
 }
--- light_bulb/src/App.jsx
+++ light_bulb/src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 
 function App() {
   return <LightBulb />;
